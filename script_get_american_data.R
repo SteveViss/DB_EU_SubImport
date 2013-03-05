bc83d7aa4f1ae379e5fec04b4e823c08c9fbf259
Steve <steve@steve-HP-530-Notebook-PC.(none)>
2013-03-05 18:59:55 -0500

    Import by files

--- script_get_american_data.R
+++ script_get_american_data.R
@@ -16,9 +16,9 @@ File_names  <- read.csv2(file.choose(),header=FALSE)
 system("mkdir Data_state") # créer un repértoire pour stocker les données
 setwd("Data_state") # je me place dans le répértoire
 
-state <- c("AZ","CO","ID","MT","NM","NV","WY","UT","AK","CA","HI","OR","WACT","DE","IA","IL","IN","KS","MA","MD","ME","MI","MN","MO","ND","NE","NH","NJ","NY","OH","PA","RI","SD","VT","WI","WV","AL","AR","FL","GA","KY","LA","MS","NC","OK","SC","TN","TX","VA") # les noms des variables ici les états
-state <- paste(state,".ZIP",sep="")
-state <- as.data.frame(state)
+# state <- c("AZ","CO","ID","MT","NM","NV","WY","UT","AK","CA","HI","OR","WACT","DE","IA","IL","IN","KS","MA","MD","ME","MI","MN","MO","ND","NE","NH","NJ","NY","OH","PA","RI","SD","VT","WI","WV","AL","AR","FL","GA","KY","LA","MS","NC","OK","SC","TN","TX","VA") # les noms des variables ici les états
+# state <- paste(state,".ZIP",sep="")
+# state <- as.data.frame(state)
 
 #IL faut charger l'ensemble de la fonction dans ta console R
 ##########################################################
@@ -28,17 +28,14 @@ state <- as.data.frame(state)
 getCSV <- function(X=state){
 
 the.command <- paste("wget 'http://apps.fs.fed.us/fiadb-downloads/",X,"'",sep="") # reconstruction de l'URL a partir de la valeur de X
-
 cat(the.command) # pour afficher l'url que l'ordi affiche
-
 system(the.command) # executer la commande par le système
-
 } # end of function get CSV
 ##########################################################
 
 un exemple pour AL_BOUNDARY.ZIP
 
-getCSV(state[1,1]) # la tu viens de récupérer le fichier
+getCSV(Files_names[1,1]) # la tu viens de récupérer le fichier
 
 # Et maintenant on applique la fonction pour tout les fichiers
  sapply(File_names,getCSV) #pour chaque éléments de File_names on applique la fonction getCSV
