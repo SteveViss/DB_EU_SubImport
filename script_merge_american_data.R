1beeaca50c509a47fcc5b111423f12b66971635e
Steve Vissault <s.vissault@yahoo.fr>
2014-04-09 23:10:26 -0400

    errors

--- script_merge_american_data.R
+++ script_merge_american_data.R
@@ -5,16 +5,16 @@
 # Auteur: Steve Vissault                   #  
 ############################################
 
-########### Merge each table (e.g. Boundaries.CSV) to one CSV file for the entire study area. 
+########### Merge each table name to one CSV file. 
 
 setwd("/Users/path/to/CSV_files/")
 
-name_tables <- read.csv(file.choose()) # List contained each tables name (see file_names_group.csv as example)
-list_csv <- list.files(getwd()) # List of every csv files containing in a specific folder. Create a folder with every csv files for each state you want to merge (by table).
+name_tables <- read.csv(file.choose()) # List on each table names (see file_names_group.csv)
+list_csv <- list.files(getwd()) # List of every csv files folder. 
 dir.create(file.path(getwd(), "Ouputs")) # Create a folder for Ouputs
 
 for (i in 1:dim(name_tables)[1]){
-  match_list <- grep(name_tables[i,1], list_csv, value=TRUE) # Find every tables with the same name from the list_csv.
+  match_list <- grep(name_tables[i,1], list_csv, value=TRUE) # list tables with the same name.
   cat(noquote(paste("Merge in progress for the table:",name_tables[i,1],"\n",sep=" ")))
   cat("----------------------\n")
   for (y in 1:length(match_list)){
@@ -24,8 +24,7 @@ for (i in 1:dim(name_tables)[1]){
           output  <- rbind(output,read.csv(match_list[y]))
         }
         if(y==length(match_list)){
-          write.csv(output,file=paste(getwd(),"/Ouputs/",name_tables[i,1],sep="")) # Write output (csv merged) in the "outputs" folder
-        }
+          write.csv(output,file=paste(getwd(),"/Ouputs/",name_tables[i,1],sep="")) # Write output (csv merged)
   }
 }
 
