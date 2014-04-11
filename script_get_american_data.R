c4b54166de09d0e2d96b3fe8443b9966d0a7a8c4
Timothée Poisot <timothee.poisot@uqar.ca>
2014-04-11 17:14:42 -0400

    Improved generality
    - check that the directory exist in a system-agnostic way
    - better `getCSV` function using httr

--- script_get_american_data.R
+++ script_get_american_data.R
@@ -5,28 +5,30 @@
 # Auteur: Camille Albouy et Steve Vissault #
 ############################################
 
+library(stringr)
+library(httr)
+
 setwd("/home/steve/Bureau/Data_state") 
 getwd()
 
 # Load file_name CSV file
 File_names  <- read.csv2(file.choose(),header=FALSE)
 
-system("mkdir Data_state") 
-setwd("Data_state") 
+if (file.exists("Data_state")){
+    setwd(file.path(getwd(), "Data_state"))
+} else {
+    dir.create(file.path(getwd(), "Data_state"))
+    setwd(file.path(getwd(), "Data_state"))
+}
 
 ##########################################################
-getCSV <- function(X=state){
-the.command <- paste("wget 'http://apps.fs.fed.us/fiadb-downloads/",X,"'",sep="") # reconstruction de l'URL a partir de la valeur de X
-cat(the.command) 
-system(the.command) 
-} # end of function get CSV
+getCSV <- function(X=state) return(httr::GET(str_c("http://apps.fs.fed.us/fiadb-downloads/",X))$content)
 ##########################################################
 
 for (i in 1:dim(File_names)[1]){
   getCSV(File_names[i,1])
   cat(i)
-         }
+}
   
 ##### Unzip files recently downloaded
 system("for i in *.ZIP; do unzip $i;done")
- 
